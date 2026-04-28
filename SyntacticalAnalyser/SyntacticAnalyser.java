4baa1fc2bc74e6c4010e18f779de01e2677f52db
Alex Easey <alexeasey@hotmail.com>
2026-04-28 22:30:09 +1000

    refactored Runner.Java for O(1) lookup system for parsing table

--- SyntacticalAnalyser/SyntacticAnalyser.java
+++ SyntacticalAnalyser/SyntacticAnalyser.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class SyntacticAnalyser {
 
 	public static ParseTree parse(List<Token> tokens) throws SyntaxException {
-		//Turn the List of Tokens into a ParseTree.
 
+		//Turn the List of Tokens into a ParseTree
 
 		String[][][] parsingTable = {
 				{null,{"PLUS"},{"MINUS"},{"TIMES"},{"DIVIDE"},{"MOD"},{"ASSIGN"},{"EQUAL"},{"NEQUAL"},{"LT"},{"GT"},{"LE"},{"GE"},{"LPAREN"},{"RPAREN"},{"LBRACE"},{"RBRACE"},{"AND"},{"OR"},{"SEMICOLON"},{"PUBLIC"},{"CLASS"},{"STATIC"},{"VOID"},{"MAIN"},{"STRINGARR"},{"ARGS"},{"TYPE"},{"PRINT"},{"WHILE"},{"FOR"},{"IF"},{"ELSE"},{"DQUOTE"},{"SQUOTE"},{"ID"},{"NUM"},{"TRUE"},{"FALSE"},{"CHARLIT"},{"STRINGLIT"}},
@@ -27,7 +27,6 @@ public class SyntacticAnalyser {
 				{{"print"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"PRINT", "LPAREN", "printexpr", "RPAREN"},null,null,null,null,null,null,null,null,null,null,null,null},
 				{{"type"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"TYPE"},null,null,null,null,null,null,null,null,null,null,null,null,null},
 				{{"expr"},null,null,null,null,null,null,null,null,null,null,null,null,{"relexpr", "boolexpr"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"charexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},null,null},
-				{{"charexpr"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"SQUOTE", "CHARLIT", "SQUOTE"},null,null,null,null,null,null},
 				{{"boolexpr"},null,null,null,null,null,null,{"boolop", "relexpr", "boolexpr"},{"boolop", "relexpr", "boolexpr"},null,null,null,null,null,{"epsilon"},null,null,{"boolop", "relexpr", "boolexpr"},{"boolop", "relexpr", "boolexpr"},{"epsilon"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 				{{"boolop"},null,null,null,null,null,null,{"booleq"},{"booleq"},null,null,null,null,null,null,null,null,{"boollog"},{"boollog"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 				{{"booleq"},null,null,null,null,null,null,{"EQUAL"},{"NEQUAL"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
@@ -40,7 +39,8 @@ public class SyntacticAnalyser {
 				{{"term"},null,null,null,null,null,null,null,null,null,null,null,null,{"factor", "termprime"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"factor", "termprime"},{"factor", "termprime"},null,null,null,null},
 				{{"termprime"},{"epsilon"},{"epsilon"},{"TIMES", "factor", "termprime"},{"DIVIDE", "factor", "termprime"},{"MOD", "factor", "termprime"},null,{"epsilon"},null,null,null,null,null,null,{"epsilon"},null,null,null,null,{"epsilon"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 				{{"factor"},null,null,null,null,null,null,null,null,null,null,null,null,{"LPAREN", "arithexpr", "RPAREN"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"ID"},{"NUM"},null,null,null,null},
-				{{"printexpr"},null,null,null,null,null,null,null,null,null,null,null,null,{"relexpr", "boolexpr"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"DQUOTE", "STRINGLIT", "DQUOTE"},null,{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},null,null}
+				{{"printexpr"},null,null,null,null,null,null,null,null,null,null,null,null,{"relexpr", "boolexpr"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"DQUOTE", "STRINGLIT", "DQUOTE"},null,{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},{"relexpr", "boolexpr"},null,null},
+				{{"charexpr"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"SQUOTE", "CHARLIT", "SQUOTE"},null,null,null,null,null,null}
 		};
 
 		if (tokens.size() == 0){ //if empty input syntax exception
@@ -57,25 +57,23 @@ public class SyntacticAnalyser {
 		output.setRoot(root);
 		TreeNode parent = root;
 		addRule(stack.pop(), tokens.get(0), stack, parsingTable);
-		addChildren(parent, "prog", tokens.get(0).getType().toString(), stack, parsingTable, tokens);
+		addChildren(parent, "prog", tokens.get(0), stack, parsingTable, tokens);
 
 		return output;
 	}
 
-	static void addChildren(TreeNode parent, String nonTerm, String terminal, Deque<String> stack, String[][][] parsingTable, List<Token> tokens) throws SyntaxException {
+	static void addChildren(TreeNode parent, String rowTarget, Token colTarget, Deque<String> stack, String[][][] parsingTable, List<Token> tokens) throws SyntaxException {
 		int num = 0;
 		String stackTop = stack.peekLast();
 		Token tokenTop = tokens.get(0);
 
-		int column = findColumn(terminal, parsingTable); //find terminal index
-		int row = findRow(nonTerm, parsingTable); //find non-terminal index
+		int column = findColumn(colTarget); //find terminal index
+		int row = findRow(rowTarget); //find non-terminal index
 
 		String s = ""; //Initialise a string the rule will be turned into
 
 		for (int i = 0; i < parsingTable[row][column].length; i++) {
 			s = parsingTable[row][column][i]; //Set the string to the rule being added
-			System.out.println(tokenTop.getValue().toString());
-			System.out.println(stack);
 			try { //tests if the current rule being added is terminal or non-terminal, if non-terminal it throws exception which is caught
 				if ((!Token.TokenType.valueOf(s).isVariable()) && s.equals(tokenTop.getType().toString())){ //isVariable() returns false so inverse of it
 					parent.addChild(new TreeNode(TreeNode.Label.terminal, tokenTop, parent));
@@ -87,11 +85,11 @@ public class SyntacticAnalyser {
 				}
 			} catch (Exception e){ //If isVariable() doesn't exist then it's either a non-terminal or error which this will catch
 				if (s != "epsilon") { //expandable non-terminal
-					TreeNode temp = new TreeNode(TreeNode.Label.valueOf(s), parent);
+					TreeNode tempNode = new TreeNode(TreeNode.Label.valueOf(s), parent);
 					stack.removeLast();
-					parent.addChild(temp);
+					parent.addChild(tempNode);
 					addRule(s, tokenTop, stack, parsingTable);
-					addChildren(temp, s, tokenTop.getType().toString(), stack, parsingTable, tokens);
+					addChildren(tempNode, s, tokenTop, stack, parsingTable, tokens);
 				} else { // epsilon
 					parent.addChild(new TreeNode(TreeNode.Label.epsilon, parent));
 					stack.removeLast();
@@ -101,15 +99,15 @@ public class SyntacticAnalyser {
 				stackTop = stack.peekLast();
 				tokenTop = tokens.get(0);
 			} else {
-				throw new SyntaxException("");
+				throw new SyntaxException(rowTarget + " " + row);
 			}
 		}
 	}
 
-	public static void addRule(String target, Token token, Deque<String> stack, String[][][] parsingTable) throws SyntaxException { //Add the rules that exist in the parsing table to the stack
+	public static void addRule(String rowTarget, Token colTarget, Deque<String> stack, String[][][] parsingTable) throws SyntaxException { //Add the rules that exist in the parsing table to the stack
 
-		int column = findColumn(token.getType().toString(), parsingTable);
-		int row = findRow(target, parsingTable);
+		int column = findColumn(colTarget);
+		int row = findRow(rowTarget);
 		String s = "";
 		try {
 			for (int i = parsingTable[row][column].length - 1; i >= 0; i--) {
@@ -117,72 +115,18 @@ public class SyntacticAnalyser {
 				stack.addLast(s);
 			}
 		} catch (Exception e) { //If there is no rule to add then wrong input
-			throw new SyntaxException("");
+		    throw new SyntaxException(rowTarget + " " + row);
 		}
 	}
 
-	public static int findColumn(String target, String[][][] parsingTable){ //find column of the terminal
-		int column = 0;
-		for (int i = 1; i<parsingTable[0].length; i++){
-			if (parsingTable[0][i][0] == target){
-				column = i;
-				break;
-			}
-		}
+	public static int findColumn(Token colToken){ //find column of the terminal
+		int column = colToken.getType().ordinal() + 1;
 		return column;
 	}
 
-	public static int findRow(String target, String[][][] parsingTable){ //Find row of the non-terminal
-		int row = 0;
-		for (int i = 1; i<= (parsingTable.length - 1); i++){
-			if (parsingTable[i][0][0] == target){
-				row = i;
-				break;
-			}
-		}
+	public static int findRow(String rowToken){ //Find row of the non-terminal
+		int row = TreeNode.Label.valueOf(rowToken).ordinal() + 1;
 		return row;
 	}
 
 }
-
-// The following class may be helpful.
-
-class Pair<A, B> {
-	private final A a;
-	private final B b;
-
-	public Pair(A a, B b) {
-		this.a = a;
-		this.b = b;
-	}
-
-	public A fst() {
-		return a;
-	}
-
-	public B snd() {
-		return b;
-	}
-
-	@Override
-	public int hashCode() {
-		return 3 * a.hashCode() + 7 * b.hashCode();
-	}
-
-	@Override
-	public String toString() {
-		return "{" + a + ", " + b + "}";
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ((o instanceof Pair<?, ?>)) {
-			Pair<?, ?> other = (Pair<?, ?>) o;
-			return other.fst().equals(a) && other.snd().equals(b);
-		}
-
-		return false;
-	}
-
-}
-
